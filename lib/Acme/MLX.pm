eb0a4acf2434d33f277398994c2b1df2d431afc8
Brian T. Wightman <midlifexis@wightmanfam.org>
2012-06-13 19:58:53 +0000

    Import of MLX/Acme-MLX-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-MLX
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         MLX/Acme-MLX-0.01.tar.gz
    gitpan-cpan-author:       MLX
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/MLX.pm
@@ -0,0 +1,111 @@
+package Acme::MLX;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Acme::MLX - The great new Acme::MLX!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Acme::MLX;
+
+    my $foo = Acme::MLX->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Brian T. Wightman, C<< <MidLifeXis at wightmanfam.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-acme-mlx at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Acme-MLX>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Acme::MLX
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Acme-MLX>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Acme-MLX>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Acme-MLX>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Acme-MLX/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2012 Brian T. Wightman.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Acme::MLX
